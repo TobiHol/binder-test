a239b50aef67773102d1368a746dbe5b703247bc
Tobias Holtdirk <tobias.holtdirk@mailbox.org>
2023-11-17 13:55:01 +0100

    Cleanup

--- download_models.py
+++ download_models.py
@@ -2,6 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from pathlib import Path
 
 MODEL_DIR = Path("models")
+
 models = ["gpt2", "facebook/opt-125m", "EleutherAI/pythia-70m"]
 
 def download_model(model_name):
@@ -16,8 +17,8 @@ def download_model(model_name):
     model = AutoModelForCausalLM.from_pretrained(model_name)
 
     # Save the model and tokenizer to the specified directory
-    model.save_pretrained(model_path)
     tokenizer.save_pretrained(model_path)
+    model.save_pretrained(model_path)
 
 for model in models:
     download_model(model)
